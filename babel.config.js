3bf497b169421ff2204011207c6a319ecd9fdd76
九宫格 <1537309430@qq.com>
2022-05-23 15:47:25 +0800

    优化

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,14 @@
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: [
-    '@babel/plugin-syntax-dynamic-import'
+    '@babel/plugin-syntax-dynamic-import',
+    ...prodPlugins
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -10,8 +10,15 @@ module.exports = {
         })
       config.module.rule('js').exclude.add(/\.worker\.js$/)
       config.output.globalObject('this')
+    config.set('externals', {
+      echarts: 'echarts',
+      // vue: 'Vue',
+      // axios: 'axios',
+      // 'vue-router': 'VueRouter',
+      // lodash: '_',
+      // 'element-plus': 'ElementPlus'
+    })
     },
     // 解决打包的时报错
-    parallel: false,
-    
+    parallel: false,    
 }
